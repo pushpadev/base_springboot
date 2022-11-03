826c185a66ffbe57b232868e15a2fe2da6994270
pushpadev <pushpa.fullstackdeveloper@gmail.com>
2022-11-03 21:24:18 +0530

    added different api points

--- main/src/main/java/com/pushpa/main/MainController.java
+++ main/src/main/java/com/pushpa/main/MainController.java
@@ -1,7 +1,6 @@
 package com.pushpa.main;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class MainController {
@@ -9,5 +8,15 @@ public class MainController {
 	@GetMapping("/")
 	public String index() {
 		return "Greetings from Pushpa!";
+	}
+
+	@RequestMapping(value = "/users/{id}", method = RequestMethod.GET)
+	public String getUser(@PathVariable("id") String id) {
+		return "Hello, " + id;
+	}
+
+	@PostMapping(value = "/users")
+	public String createUser(String user) {
+		return "Create " + user;
 	}
 }
